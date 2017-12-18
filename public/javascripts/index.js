c1c7d5e5ef975570b1c753eea9fa383caa06af04
nqdao <nhat.quang.d@gmail.com>
2017-12-18 12:42:54 -0500

    typo

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
   var txtbox = document.createElement("input");
   txtbox.type = "text";
   txtbox.setAttribute('readonly',true);
-  txtbox.value = "Event Hub"
+  txtbox.value = "Event Hub";
   var body = document.getElementsByTagName('body')[0];
   body.appendChild(txtbox);
 
